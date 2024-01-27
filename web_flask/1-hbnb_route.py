edd11ab3abba02bdd2fa184348ba95782b954ad5
jsump <libbyn79@gmail.com>
2024-01-27 00:15:18 -0800

    HBNB route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module: 1-hbnb_route
+
+This module contains a function that starts a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    This retuens the message after defining the route for the application
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This returns the message after defining the route for hbnb
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    """
+    Flask application run on 0.0.0.0 on port 5000
+    """
+    app.run(host='0.0.0.0', port=5000)
